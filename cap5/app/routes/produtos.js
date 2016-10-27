e8edbebeb81aa63102ce03955f9940dee4f065e2
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-10-27 19:35:42 -0200

    capitulo 5: Respeitando o accept [implementando validações nos dados do form.ejs]

---
+++ cap5/app/routes/produtos.js
@@ -0,0 +1,52 @@
+module.exports = function (app) {
+    app.get('/produtos', function (req, res) {
+        //pagando conexao
+        var connection = app.infra.connectionFactory();
+        //consulta
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.lista(function (err, results) {
+            res.format({
+                html: function () {
+                    res.render('produtos/lista', { lista: results });
+                },
+                json: function () {
+                    res.json(results);
+                }
+            });
+        });
+        connection.end();
+    });
+
+    app.get('/produtos/form', function (req, res) {
+        res.render('produtos/form', { errosValidacao: {}, produto: {} });
+    });
+
+    app.post('/produtos', function (req, res) {
+        var produto = req.body;
+
+        // express validator
+        req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        req.assert('preco', 'Formato inválido').isFloat();
+        var erros = req.validationErrors();
+        if (erros) {
+            res.format({
+                //status 400 = BadRequest
+                html: function () {
+                    res.status(400).render('produtos/form', { errosValidacao: erros, produto: produto });
+                },
+                json: function () {
+                    res.status(400.).json(erros);
+                }
+            });
+            return;
+        }
+
+        //pagando conexao
+        var connection = app.infra.connectionFactory();
+        //consulta
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function (err, results) {
+            res.redirect('/produtos');
+        });
+    });
+}
