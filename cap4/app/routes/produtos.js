e8efa3bd0f065f94a3083b5f3961d553f7811b37
Paulo-Sergio <paulofranca.contato@gmail.com>
2016-10-26 23:27:28 -0200

    capitulo 4 - Content negotiation

---
+++ cap4/app/routes/produtos.js
@@ -0,0 +1,35 @@
+module.exports = function (app) {
+    app.get('/produtos', function (req, res) {
+        //pagando conexao
+        var connection = app.infra.connectionFactory();
+        //consulta
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.lista(function (err, results) {
+            res.format({
+                html: function () {
+                    res.render('produtos/lista', { lista: results });
+                },
+                json: function () {
+                    res.json(results);
+                }
+            });
+        });
+        connection.end();
+    });
+
+    app.get('/produtos/form', function (req, res) {
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos', function (req, res) {
+        var produto = req.body;
+
+        //pagando conexao
+        var connection = app.infra.connectionFactory();
+        //consulta
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function (err, results) {
+            res.redirect('/produtos');
+        });
+    });
+}
